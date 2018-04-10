e77922e615c6807188416cd4aa26a6aa7be8552e
on <you@example.com>
2018-04-11 00:13:12 +0200

    upgrades

--- pseudomatrix.hs
+++ pseudomatrix.hs
@@ -1,14 +1,29 @@
 
-mat1 = [[1,2,3,4],[5,1,2,3],[5,4,3,1],[12,11,3,1]]
-mat2 = [[1,2,3,4],[5,1,2,3],[5,4,3,1],[12,11,3,2]]
+mat1 = [[1.0,2,3,4],[5,1,2,3],[5,4,3,1],[12,11,3,1]]
+mat2 = [[1.0,2,3,4],[5,1,2,3],[5,4,3,1],[12,11,3,2]]
 mat3=[[1,2,3,4]]
 
-v1 = [1,2,4,5]
+v1 = [1,2,4,5.0]
 v2 = [5,4,3,1]
-     
+
+norm v = sqrt $ dprod v v
+
+unit v = map (\p -> p/(norm v)) v
+         
 transpose ([]:_) = []
 transpose rows@(r:ws) = (map head rows):(transpose $ map tail rows)
 
+rowcat mat1 mat2 = mat1 ++ mat2
+colcat mat1 mat2 = zipWith (++) mat1 mat2
+
+size m@(a:t) = (length m,length a)
+size _ = (0,0)
+         
+blkdiag mat1 mat2 =
+     let (n1,m1) = size mat1
+         (n2,m2) = size mat2
+     in rowcat (colcat mat1 (zeros n1 m2))
+               (colcat (zeros n2 m1) mat2)
 
 dprod v1 v2 = foldl (\s (p1,p2) -> s+p1*p2) 0 (zip v1 v2)
 
@@ -16,9 +31,9 @@ mmul m1 m2 =
       let m2' = transpose m2
       in map (\column -> map (\row ->  dprod row column) m1) m2'
 
-vals  n m val = replicate n $ replicate m val
-zeros = vals 0
-ones  = vals 1
+vals val  n m  = replicate n $ replicate m val
+zeros = vals (0.0::Double)
+ones  = vals (1.0::Double)
 
 eye n = let bigrow = concat $ repeat $ 1:(replicate n 0)
             take' n inp = take n inp : take' n (drop n inp)
